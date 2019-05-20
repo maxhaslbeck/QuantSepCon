973ffa34390c59582abe19f3c1a40577e69e4c9c
Maximilian PL Haslbeck <haslbema@in.tum.de>
2019-05-20 23:35:45 +0200

    quantSepCon stub

---
+++ QuantSepCon.thy
@@ -0,0 +1,222 @@
+(*
+  Author: Maximilian P. L. Haslbeck
+  Author: 
+*)
+theory QuantSepCon
+  imports
+  "Sep_Algebra_Add"
+ "Separation_Algebra.Separation_Algebra" "HOL-Library.Extended_Nat"
+  "HOL-Library.Extended_Nonnegative_Real" 
+begin
+
+
+definition emb :: "('b \<Rightarrow> bool) \<Rightarrow> 'b  \<Rightarrow> ennreal" where
+  "emb P x = (if P x then 1 else 0)"
+
+
+context sep_algebra
+begin
+
+
+definition
+  sep_empty_q :: "'a \<Rightarrow> ennreal"  where
+  "sep_empty_q \<equiv> emb (\<lambda>h. h = 0)"
+
+subsection \<open>Quantitative Separating Conjunction\<close>
+
+definition
+  sep_conj_q :: "('a \<Rightarrow> ennreal) \<Rightarrow> ('a \<Rightarrow> ennreal) \<Rightarrow> ('a \<Rightarrow> ennreal)" (infixr "**q" 35)
+  where
+  "P **q Q \<equiv> \<lambda>h. Sup { P x * Q y | x y. h=x+y \<and> x ## y}" (* why not Sup ? *)
+ 
+
+lemma sep_conj_q_alt : "(P **q Q) = (\<lambda>h. (SUP (x,y): {(x,y). h=x+y \<and> x ## y}. P x * Q y))"
+  unfolding  sep_conj_q_def
+  apply auto apply(rule ext)
+  apply(rule arg_cong[where f=Sup]) by auto
+
+
+thm Sup_mono
+
+term "Sup (f ` S)"
+term "SUP s:S. f s"
+
+find_theorems name: Sup_cong
+lemma (in -) Sup_cong: "\<And>S S'. S=S' \<Longrightarrow> Sup S = Sup S'"
+  by simp
+find_theorems name: Sup_cong
+
+thm SUP_cong
+
+lemma "Sup (f ` S) = (SUP s:S. f s)"
+  apply(rule arg_cong[where f=Sup]) by simp
+
+lemmas Sup_cong2 = arg_cong[where f=Sup]
+thm Sup_cong2
+find_theorems "Sup"
+
+lemma "Sup (f ` S) = (SUP s:S. f s)"
+  apply(rule Sup_cong) by simp
+
+
+
+
+lemma sep_conj_q_impl1:
+  assumes P: "\<And>h. P h \<le> I h"
+  shows "(P **q R) h \<le> (I **q R) h"
+  unfolding sep_conj_q_alt
+  sorry
+
+
+
+lemma sep_conj_q_impl :
+  assumes P: "\<And>h. P h \<le> P' h"
+  assumes Q: "\<And>h. Q h \<le> Q' h"
+  shows "(P **q Q) h \<le> (P' **q Q') h"
+  sorry
+
+
+
+lemma sep_conj_q_SUP: "(P **q Q) = (\<lambda>h. (SUP i:{(x,y)| x y. h=x+y \<and> x ## y}. (\<lambda>(x,y). P x * Q y) i))"
+  unfolding sep_conj_q_def apply auto  apply (rule ext)
+  apply(rule Sup_cong) by auto 
+
+
+
+subsection \<open>Quantitative Separating Implication - Magic Wand\<close>
+
+definition
+  sep_impl_q :: "('a \<Rightarrow> bool) \<Rightarrow> ('a \<Rightarrow> ennreal) \<Rightarrow> ('a \<Rightarrow> ennreal)" (infixr "-*q" 35)
+  where
+  "P -*q Q \<equiv> undefined"
+
+
+
+subsection \<open>Properties of Quantitative Separating Connectives\<close> 
+
+subsubsection \<open>Commutative monoid\<close>
+
+lemma star_assoc: "(x **q (y **q z))  = ((x **q y) **q z) "
+proof (rule ext)
+  fix h
+  have "(x **q (y **q z)) h    = (SUP (xa, ya):{(x, y) |x y. h = x + y \<and> x ## y}. x xa * (SUP (x, ya):{(x, y) |x y. ya = x + y \<and> x ## y}. y x * z ya))" 
+    unfolding sep_conj_q_SUP by auto 
+  also have "\<dots> = (SUP xa:{(x, y). h = x + y \<and> x ## y}. case xa of (xa, ya) \<Rightarrow> SUP i:{(x, y). ya = x + y \<and> x ## y}. (case i of (h21, h22) \<Rightarrow> x xa * y h21 * z h22))"
+    by(simp add: SUP_mult_left_ennreal prod.case_distrib mult.assoc)
+ 
+  also have "\<dots> = (SUP xa:{(x, y). h = x + y \<and> x ## y}. SUP i:{((fst xa),x, y)| x y . snd xa = x + y \<and> x ## y}. (case i of (b, h21, h22) \<Rightarrow> x b * y h21 * z h22))"
+    apply(rule SUP_cong) apply simp
+    apply safe
+    apply(rule arg_cong[where f=Sup]) apply safe
+    subgoal by force 
+    subgoal  apply auto done
+    done
+  also have "\<dots> = (SUP xa:{(h1, h2, h3). h = h1 + h2 + h3 \<and> h1 ## h2 + h3 \<and> h1 ## h2 \<and> h1 ## h3 \<and> h3 ## h2 }. case xa of (h1, h2, h3) \<Rightarrow> x h1 * y h2 * z h3)"
+    apply(subst SUP_UNION[symmetric]) 
+    apply(rule SUP_cong)
+    subgoal apply (auto simp: sep_add_assoc sep_conj_assoc sep_disj_commute dest: sep_disj_addD )
+      subgoal  
+        by (metis  sep_add_assoc  sep_disj_addD1  sep_disj_addD2) 
+      done
+    apply auto done
+  also have "\<dots> = (SUP xa:{(x, y). h = x + y \<and> x ## y}. SUP i:{(h1,h2, snd xa)| h1 h2 . fst xa = h1 + h2 \<and> h1 ## h2}. (case i of (h1, h2, h3) \<Rightarrow> x h1 * y h2 * z h3))"
+    apply(subst SUP_UNION[symmetric]) 
+    apply(rule SUP_cong)
+    subgoal apply (auto simp: sep_add_assoc sep_conj_assoc sep_disj_commute dest: sep_disj_addD )
+      subgoal   
+        using  sep_disj_addI1  sep_disj_commuteI by blast   
+      subgoal   
+        using sep_disj_addI3 sep_disj_commuteI by blast   
+      done
+    apply auto done
+  also have "\<dots> = (SUP xa:{(h12, h3). h = h12 + h3 \<and> h12 ## h3}. case xa of (h12, h3) \<Rightarrow> SUP h12:{(x, y). h12 = x + y \<and> x ## y}. (case h12 of (h1, h2) \<Rightarrow> (x h1 * y h2 * z h3)))"
+    apply(rule SUP_cong) apply simp
+    apply safe
+    apply(rule arg_cong[where f=Sup]) apply safe
+    subgoal by force 
+    subgoal by force 
+    done 
+  also have "\<dots> = ((x **q y) **q z) h"
+    unfolding sep_conj_q_SUP apply  (auto simp:  SUP_mult_right_ennreal) 
+    apply(rule SUP_cong) apply simp
+    apply auto apply(rule SUP_cong) by (auto simp: mult.assoc) 
+  finally show "(x **q (y **q z)) h  = ((x **q y) **q z) h " .
+qed
+
+
+lemma emp_neutral[sep_algebra_simps]:
+  "(X **q sep_empty_q) = X"
+  "(sep_empty_q **q X) = X"
+  sorry
+
+
+lemma star_comm: "(X **q Y) = (Y **q X)"
+  sorry
+
+
+lemma sep_conj_q_left_commute: "(P **q Q **q R) = (Q **q P **q R)"
+  apply(subst  star_assoc)
+  apply(subst  star_comm)
+  apply(subst  star_assoc) by simp
+
+
+lemmas sep_conj_q_c = star_comm sep_conj_q_left_commute
+
+
+subsubsection \<open>(Sub)distributivity Laws\<close>
+
+lemma theorem_3_6:
+  "(P **q (sup Q R)) = sup (P **q Q) (P **q R)"
+  "(P **q (Q + R)) \<le> (P **q Q) + (P **q R)"
+proof -
+  have "\<And>f q x. sup f q x = sup (f x) (q x)" by simp
+  have A: "\<And>a b :: ennreal. sup a b = Sup ({a} \<union> {b})"  
+    apply(subst Sup_union_distrib) by simp
+
+  have supmax: "\<And>x y::ennreal. sup x y = max x y" 
+    by (simp add: antisym)
+
+  have "\<And> a b c. (a::ennreal) * max b c = max (a*b) (a*c)" 
+    unfolding max_def apply auto  
+     apply (auto simp add: mult_left_mono) 
+    by (smt ennreal_mult_less_top ennreal_mult_strict_right_mono ennreal_mult_top ennreal_zero_less_mult_iff le_less mult.commute not_less top.extremum)
+ 
+  have sup_times_distrib: "\<And> a b c. (a::ennreal) * sup b c = sup (a*b) (a*c)" 
+    unfolding supmax by fact
+
+      
+  { fix h
+  have "(P **q (sup Q R)) h = Sup {P x * sup Q R y |x y. h = x + y \<and> x ## y}"
+  
+    unfolding sep_conj_q_def by simp
+  also have "\<dots> = Sup {P x * sup (Q y) (R y) |x y. h = x + y \<and> x ## y}"
+    by simp
+  also have "\<dots> = Sup { sup (P x * Q y) (P x * R y) |x y. h = x + y \<and> x ## y}"
+    apply(subst  sup_times_distrib)  by simp
+  also have "\<dots> = (SUP x:{(x, y). h = x + y \<and> x ## y}. case x of (x,y) \<Rightarrow> sup (P x * Q y) (P x * R y))" 
+    apply (rule arg_cong[where f=Sup]) by auto
+  also have "\<dots> = (SUP x:{(x, y). h = x + y \<and> x ## y}. sup (P (fst x) * Q (snd x)) (P (fst x) * R (snd x)))"
+    apply (rule arg_cong[where f=Sup])  
+    by (meson prod.case_eq_if)    
+  also have "\<dots> = sup (SUP x:{(x, y). h = x + y \<and> x ## y}. P (fst x) * Q (snd x))
+     (SUP x:{(x, y). h = x + y \<and> x ## y}. P (fst x) * R (snd x))"
+    apply(subst SUP_sup_distrib[symmetric])
+    subgoal apply auto apply(rule exI[where x=h])  apply(rule exI[where x=0]) by auto
+    subgoal by auto
+    subgoal by auto
+    subgoal by auto
+    done 
+  also have "\<dots> = sup (P **q Q) (P **q R) h"
+    unfolding sep_conj_q_alt apply simp
+     
+    by (metis (mono_tags, lifting) SUP_cong prod.case_eq_if)  
+  finally have "(P **q sup Q R) h = sup (P **q Q) (P **q R) h ".
+}
+  then show "(P **q (sup Q R)) = sup (P **q Q) (P **q R)" by auto
+
+next
+  show "(P **q Q + R) \<le> (P **q Q) + (P **q R)" 
+    sorry
+qed
+
+end
---
+++ Sep_Algebra_Add.thy
@@ -0,0 +1,236 @@
+(* Author: Peter Lammich *)
+theory Sep_Algebra_Add
+imports "Separation_Algebra.Sep_Tactics"
+begin
+
+section \<open>Simp-Lemmas for Separation Algebra\<close>
+
+named_theorems sep_algebra_simps \<open>Advanced simplification lemmas for separation algebra\<close>
+
+lemmas [sep_algebra_simps] = sep_conj_exists
+lemmas [simp] = sep_conj_assoc
+
+lemmas sep_conj_c = sep_conj_commute sep_conj_left_commute
+
+
+section \<open>Separation Algebra with a Unique Zero\<close>
+
+class unique_zero_sep_algebra = sep_algebra +
+  assumes unique_zero: "x##x \<Longrightarrow> x=0"
+begin
+  lemma unique_zero_simps[simp]: 
+    "a##a \<longleftrightarrow> a=0"
+    "a##b \<Longrightarrow> a+b = a \<longleftrightarrow> b=0"
+    "a##b \<Longrightarrow> a+b = b \<longleftrightarrow> a=0"
+    "a##b \<Longrightarrow> a + b = 0 \<longleftrightarrow> a=0 \<and> b=0"
+    apply -
+    using local.sep_disj_zero local.unique_zero apply blast
+    apply (metis local.sep_add_disjD local.sep_add_zero local.unique_zero)
+    apply (metis local.sep_add_disjD local.sep_add_zero_sym local.sep_disj_commute local.unique_zero)
+    by (metis local.sep_add_disjD local.sep_add_zero local.sep_disj_zero local.unique_zero)
+
+end
+
+section \<open>Standard Instantiations\<close>
+
+instantiation "fun" :: (type,plus) plus
+begin
+  definition [simp]: "(f1 + f2) x = (f1 x + f2 x)"
+  instance
+    by standard
+end
+
+instantiation "fun" :: (type,sep_algebra) sep_algebra
+begin
+  definition "f1 ## f2 \<longleftrightarrow> (\<forall>x. f1 x ## f2 x)"
+ (* definition [simp]: "(f1 + f2) x = (f1 x + f2 x)" *)
+  definition [simp]: "0 x \<equiv> 0"
+
+  instance
+    apply standard
+    unfolding sep_disj_fun_def plus_fun_def zero_fun_def
+    apply (auto)
+    subgoal
+      by (simp add: sep_disj_commute)
+    subgoal
+      using sep_add_commute by auto
+    subgoal 
+      by (simp add: sep_add_assoc) 
+    subgoal 
+      using sep_disj_addD1 by blast 
+    subgoal 
+      by (simp add: sep_disj_addI1) 
+    done  
+  
+end
+
+instance "fun" :: (type,unique_zero_sep_algebra) unique_zero_sep_algebra
+  apply standard
+  unfolding sep_disj_fun_def plus_fun_def zero_fun_def
+  by (metis unique_zero)
+  
+lemma disj_fun_upd_iff[simp]: 
+  "\<lbrakk>f a = 0; g a = 0\<rbrakk> \<Longrightarrow> (f(a:=x) ## g(a:=y)) \<longleftrightarrow> (f ## g \<and> x ## y)"
+  unfolding sep_disj_fun_def by force
+
+
+instantiation option :: (sep_algebra) sep_algebra begin
+
+  fun sep_disj_option :: "'a option \<Rightarrow> 'a option \<Rightarrow> bool" where
+    "sep_disj None None \<longleftrightarrow> True"
+  | "sep_disj None (Some _) \<longleftrightarrow> True"  
+  | "sep_disj (Some _) None \<longleftrightarrow> True"
+  | "sep_disj (Some x) (Some y) \<longleftrightarrow> x##y"
+
+  lemma sep_disj_option_add_simps[simp]: 
+    "sep_disj None x" 
+    "sep_disj x None" 
+    by (cases x; simp)+
+        
+  fun plus_option :: "'a option \<Rightarrow> 'a option \<Rightarrow> 'a option" where 
+    "plus None None = None"
+  | "plus None (Some x) = Some x"
+  | "plus (Some x) None = Some x" 
+  | "plus (Some x) (Some y) = Some (x+y)"
+
+  lemma plus_option_add_simps[simp]: "plus None x = x" "plus x None = x"
+    by (cases x; simp)+
+  
+  definition "0 = None"
+    
+  instance 
+    apply standard
+    apply (simp_all add: zero_option_def)
+    apply (case_tac x; case_tac y; simp add: sep_disj_commute)
+    apply (case_tac x; case_tac y; auto simp add: sep_disj_commute sep_add_commute)
+    apply (case_tac x; case_tac y; case_tac z; simp add: sep_add_assoc)
+    apply (case_tac x; case_tac y; case_tac z; auto dest: sep_disj_addD1)
+    apply (case_tac x; case_tac y; case_tac z; auto dest: sep_disj_addI1)
+    done    
+  
+end
+
+
+instantiation prod :: (sep_algebra, sep_algebra) sep_algebra
+begin
+  definition "a##b \<longleftrightarrow> fst a ## fst b \<and> snd a ## snd b"
+  definition "a+b = (fst a + fst b, snd a + snd b)"  
+  definition "0 = (0,0)"
+  
+  instance
+    apply standard
+    unfolding sep_disj_prod_def plus_prod_def zero_prod_def
+    by (auto 
+      simp: sep_add_ac 
+      intro: sep_disj_commuteI sep_disj_addD1 sep_disj_addI1)
+end
+
+lemma sep_disj_prod_lower[sep_algebra_simps]: "(a,b) ## (c,d) \<longleftrightarrow> a##c \<and> b##d"
+  by (simp add: sep_disj_prod_def)
+  
+lemma plus_prod_lower[sep_algebra_simps]: "(a,b) + (c,d) = (a+c,b+d)"
+  by (simp add: plus_prod_def)
+
+instance prod :: (unique_zero_sep_algebra,unique_zero_sep_algebra) unique_zero_sep_algebra
+  by standard (auto simp: zero_prod_def sep_algebra_simps)
+
+lemma prod_Z_lower[sep_algebra_simps]: 
+  "(a,b) = 0 \<longleftrightarrow> a=0 \<and> b=0"
+  "0 = (a,b) \<longleftrightarrow> a=0 \<and> b=0"
+  by (auto simp: zero_prod_def)
+
+lemma plus_prod_eqI: "(a+b,c+d) = (a,c) + (b,d)" by (simp add: sep_algebra_simps)
+      
+
+datatype 'a tsa_opt = ZERO | TRIV (the_tsa: 'a)
+
+instantiation tsa_opt :: (type) unique_zero_sep_algebra begin
+  definition sep_disj_tsa_opt :: "'a tsa_opt \<Rightarrow> 'a tsa_opt \<Rightarrow> bool" 
+    where "a##b \<longleftrightarrow> a=ZERO \<or> b=ZERO"
+
+  definition "a+b \<equiv> (case (a,b) of (ZERO,x) \<Rightarrow> x | (x,ZERO) \<Rightarrow> x)"
+  definition "0 = ZERO"
+
+  instance
+    apply standard
+    by (auto simp: zero_tsa_opt_def sep_disj_tsa_opt_def plus_tsa_opt_def split: tsa_opt.splits)
+  
+end
+  
+instance tsa_opt :: (type) cancellative_sep_algebra
+  apply standard
+  by (auto simp: zero_tsa_opt_def sep_disj_tsa_opt_def plus_tsa_opt_def split: tsa_opt.splits)
+  
+
+(* Lowering lemmas. If one side of operation breaks abstraction level, 
+  the other side is also lowered
+*)  
+lemma TRIV_not_zero[simp]: "TRIV x \<noteq> 0" "0\<noteq>TRIV x" by (auto simp: zero_tsa_opt_def)
+lemma TRIV_Z_lower[intro!]: 
+  "ZERO = 0" 
+  "0 = ZERO" 
+  by (auto simp: zero_tsa_opt_def)  
+  
+lemma triv_Z_lower2[sep_algebra_simps]:  
+  "x + ZERO = x"
+  "ZERO + x = x"
+  by (auto simp: zero_tsa_opt_def plus_tsa_opt_def split: tsa_opt.splits)  
+  
+lemma TRIV_disj_simps[sep_algebra_simps]:
+  "TRIV a ## b \<longleftrightarrow> b=ZERO"
+  "b ## TRIV a \<longleftrightarrow> b=ZERO"
+  by (auto simp: sep_disj_tsa_opt_def)
+  
+section \<open>Additional Simplifier Setup\<close>
+
+(* 
+  This may cause trouble on non-eta-normalized terms.
+    (\<lambda>s. \<box> s) \<rightarrow> \<lambda>s. s=0
+  The next lemma hopefully fixes that.
+*)    
+
+lemma sep_empty_app[sep_algebra_simps]: "\<box> h \<longleftrightarrow> h=0"
+  by (simp add: sep_empty_def)
+
+lemma sep_empty_I[sep_algebra_simps]: "(\<lambda>h. h=0) = \<box>"
+  by (simp add: sep_empty_def)
+  
+  
+section \<open>Pure Assertions\<close>
+
+text \<open>The Separation Algebra entry only defines @{term "\<langle>\<Phi>\<rangle>"}, which
+  makes no constraint on the heap. We define \<open>\<up>\<Phi>\<close>, which requires an empty heap, 
+  and is more natural when using separation logic with deallocation.
+\<close>      
+
+definition pred_lift :: "bool \<Rightarrow> 'a::sep_algebra \<Rightarrow> bool" ("\<up>_" [100] 100)
+  where "(\<up>\<Phi>) s \<equiv> \<Phi> \<and> \<box> s"
+
+text \<open>Relation between \<open>pred_lift\<close> and \<open>pred_K\<close>. Warning, do not use 
+  as simp-lemma, as \<open>sep_true\<close> is an abbreviation for \<open>\<langle>True\<rangle>\<close>\<close>  
+lemma "\<langle>\<Phi>\<rangle> = (\<up>\<Phi> ** sep_true)" by (auto simp: pred_lift_def sep_conj_def sep_algebra_simps) 
+  
+lemma pred_lift_move_front_simps[sep_algebra_simps]:
+  "(\<up>\<Phi> ** \<up>\<Psi>) = (\<up>(\<Phi>\<and>\<Psi>))"
+  "(\<up>\<Phi> ** \<up>\<Psi> ** B) = (\<up>(\<Phi>\<and>\<Psi>) ** B)"
+  "NO_MATCH (\<up>X) A \<Longrightarrow> (A ** \<up>\<Phi> ** B) = (\<up>\<Phi> ** A ** B)"
+  "NO_MATCH (\<up>X) A \<Longrightarrow> (A ** \<up>\<Phi>) = (\<up>\<Phi> ** A)"
+  by (auto simp: pred_lift_def sep_conj_def sep_algebra_simps)
+  
+lemma pred_lift_extract_simps:
+  "(\<up>\<Phi>) h \<longleftrightarrow> \<Phi> \<and> h=0"
+  "(\<up>\<Phi> ** A) h \<longleftrightarrow> \<Phi> \<and> A h"
+  by (auto simp: pred_lift_def sep_conj_def sep_algebra_simps)
+  
+lemma pure_true_conv[sep_algebra_simps]: "\<up>True = \<box>" by (auto simp: sep_algebra_simps pred_lift_extract_simps)
+
+lemma pred_lift_false_conv[simp]: "\<up>False = sep_false" by (auto simp: sep_algebra_simps pred_lift_extract_simps)
+
+
+lemmas sep_conj_false_simps =
+  pred_lift_false_conv
+  sep_conj_false_left sep_conj_false_right
+
+
+
+end
